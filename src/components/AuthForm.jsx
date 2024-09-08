12a21f7e2e097917e354134429db984631b16856
Ankit Karnwal <karnwalankit89@gmail.com>
2024-09-09 03:50:22 +0530

    Password show hide button added

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,7 +1,16 @@
+"use client";
 import { FaLock } from "react-icons/fa";
 import styles from "./AuthForm.module.scss";
 import Link from "next/link";
+import { FaRegEye, FaEyeSlash } from "react-icons/fa";
+import { useState } from "react";
+import { FaEarDeaf } from "react-icons/fa6";
+
 export default function AuthForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  function handleEyeClick() {
+    setShowPassword(!showPassword);
+  }
   return (
     <form className={styles.form}>
       <div className={styles.form__icon}>
@@ -19,17 +28,22 @@ export default function AuthForm() {
           className={styles.form__fields__input}
         />
       </p>
-      <p className={styles.form__fields}>
+      <p className={`${styles.form__fields} ${styles.form__fieldRel} `}>
         <label htmlFor="password" className={styles.form__fields__label}>
           Password
         </label>
         <input
-          type="password"
+          type={`${showPassword ? "text" : "password"}`}
           name="password"
           id="password"
           placeholder="Example@123"
           className={styles.form__fields__input}
         />
+        {showPassword ? (
+          <FaEyeSlash onClick={handleEyeClick} />
+        ) : (
+          <FaRegEye onClick={handleEyeClick} />
+        )}
         <span>Password should be 8 characters long.</span>
       </p>
       <button type="submit">Create Account</button>
