9f67bdd75f0f8e594bc52ba5daeb634adcb7d72f
Ankit Karnwal <karnwalankit89@gmail.com>
2024-09-08 22:17:32 +0530

    skeleton modified

--- src/skeleton/CardSkeleton.jsx
+++ src/skeleton/CardSkeleton.jsx
@@ -7,7 +7,7 @@ export default function CardSkeleton() {
       <div className={styles.container__image}>
         <FaImage />
       </div>
-      <SkeletonMain width={"20rem"} height={20} />
+      <SkeletonMain height={20} />
       <SkeletonMain width={"10rem"} height={15} />
       <SkeletonMain height={20} />
     </div>
