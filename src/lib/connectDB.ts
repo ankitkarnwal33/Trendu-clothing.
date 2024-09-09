54701d6ebea12012562ad596860edfccc57630ea
Ankit Karnwal <karnwalankit89@gmail.com>
2024-09-09 15:59:39 +0530

    kjjhlw

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -12,10 +12,7 @@ export default async function connectDB() {
   }
 
   try {
-    const db = await mongoose.connect(
-      "mongodb+srv://viperankityt:b4TLrh3Msucw8akh@trendu.b9bvy.mongodb.net/?retryWrites=true&w=majority&appName=trendu",
-      {}
-    );
+    const db = await mongoose.connect(process.env.MONGO_URI_LOCAL, {});
     if (
       db &&
       db.connections &&
