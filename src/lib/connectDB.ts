9fbb097878d5feca867b1a7c5772dcea38d1c0c0
Ankit Karnwal <karnwalankit89@gmail.com>
2024-09-18 01:01:38 +0530

    solved

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -11,8 +11,8 @@ export default async function connectDB() {
   }
 
   try {
-    // const db = await mongoose.connect(process.env.MONGO_URI || "", {}); //Production
-    const db = await mongoose.connect(process.env.MONGO_URI_LOCAL || "", {}); //Localhost Dev
+    const db = await mongoose.connect(process.env.MONGO_URI || "", {}); //Production
+    // const db = await mongoose.connect(process.env.MONGO_URI_LOCAL || "", {}); //Localhost Dev
     if (db && db.connections && db.connections[0]) {
       //ReadyState => Return whether db is ready (Runnning ) or not
       connection.isConnected = db.connections[0].readyState; //Set the ready state
