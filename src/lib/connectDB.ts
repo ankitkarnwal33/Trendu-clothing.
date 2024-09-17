8822e97a4e2d46d92f5929e29ac25cfec174ecda
Ankit Karnwal <karnwalankit89@gmail.com>
2024-09-17 18:45:20 +0530

    solved

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -4,21 +4,21 @@ type ConnectionObject = {
 };
 const connection: ConnectionObject = { isConnected: 0 };
 
-export default async function connectDB(): Promise<void | string> {
+export default async function connectDB() {
   if (connection.isConnected) {
     //Database is already connected.
     return;
   }
 
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {}); //Production
+    const db = await mongoose.connect(process.env.MONGO_URI || "", {}); //Production
     // const db = await mongoose.connect(process.env.MONGO_URI_LOCAL, {}); //Localhost Dev
     if (db && db.connections && db.connections[0]) {
       //ReadyState => Return whether db is ready (Runnning ) or not
       connection.isConnected = db.connections[0].readyState; //Set the ready state
       // console.log("Database has been connected");
     }
-  } catch (error: any) {
-    return error.message;
+  } catch (error) {
+    if (error instanceof Error) return error.message;
   }
 }
